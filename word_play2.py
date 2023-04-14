253868d9a3db38a0d2978bea38bda299e03ee887
R <107578323+blue-slushy9@users.noreply.github.com>
2023-04-14 06:35:44 -0700

    Add files via upload

---
+++ word_play2.py
@@ -0,0 +1,26 @@
+﻿# New exercise, similar to wordle
+
+# g means same letter in same position
+
+# b means letter not in word at all
+
+# y means letter is in word but NOT in that position
+
+# pattern = "gbbby"
+
+guess = "raise"
+
+f = open('words.txt','r')
+
+possibles = []
+
+for possible in f:
+    if guess[0] == possible[0]: 
+        if not set(guess[1:4]).intersection(set(possible)):        
+            if guess[4] in possible: 
+                if guess[4] != possible[4]:
+                    if guess.count('e') == possible.count('e'):
+                        possible = possible.strip()
+                        possibles.append(possible)
+
+print(possibles)
